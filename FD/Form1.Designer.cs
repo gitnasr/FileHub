377bbf63c63bc99fd02bdb8b11eeb8b40c631795
Mahmoud Nasr <239.nasr@gmail.com>
2025-02-11 17:52:31 +0200

    Add project files.

---
+++ FD/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace FD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Path1 = new TextBox();
+            listBox1 = new ListBox();
+            moveToRight = new Button();
+            moveToLeft = new Button();
+            listBox2 = new ListBox();
+            Path2 = new TextBox();
+            copyTo = new Button();
+            back = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // Path1
+            // 
+            Path1.Enabled = false;
+            Path1.Location = new Point(12, 12);
+            Path1.Name = "Path1";
+            Path1.Size = new Size(213, 27);
+            Path1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(14, 53);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(208, 344);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            listBox1.MouseDoubleClick += listBox1_MouseDoubleClick;
+            // 
+            // moveToRight
+            // 
+            moveToRight.Location = new Point(243, 179);
+            moveToRight.Name = "moveToRight";
+            moveToRight.Size = new Size(78, 29);
+            moveToRight.TabIndex = 2;
+            moveToRight.Text = ">";
+            moveToRight.UseVisualStyleBackColor = true;
+            // 
+            // moveToLeft
+            // 
+            moveToLeft.Location = new Point(243, 214);
+            moveToLeft.Name = "moveToLeft";
+            moveToLeft.Size = new Size(78, 29);
+            moveToLeft.TabIndex = 3;
+            moveToLeft.Text = "<";
+            moveToLeft.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(338, 53);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(208, 344);
+            listBox2.TabIndex = 4;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            listBox2.MouseDoubleClick += listBox2_MouseDoubleClick;
+            // 
+            // Path2
+            // 
+            Path2.Enabled = false;
+            Path2.Location = new Point(333, 12);
+            Path2.Name = "Path2";
+            Path2.Size = new Size(213, 27);
+            Path2.TabIndex = 5;
+            // 
+            // copyTo
+            // 
+            copyTo.Location = new Point(14, 404);
+            copyTo.Name = "copyTo";
+            copyTo.Size = new Size(211, 34);
+            copyTo.TabIndex = 6;
+            copyTo.Text = "Copy";
+            copyTo.UseVisualStyleBackColor = true;
+            // 
+            // back
+            // 
+            back.Location = new Point(333, 404);
+            back.Name = "back";
+            back.Size = new Size(211, 34);
+            back.TabIndex = 7;
+            back.Text = "BACK";
+            back.UseVisualStyleBackColor = true;
+            back.Click += back_Click;
+            // 
+            // button5
+            // 
+            button5.ForeColor = Color.Firebrick;
+            button5.Location = new Point(16, 440);
+            button5.Name = "button5";
+            button5.Size = new Size(530, 60);
+            button5.TabIndex = 8;
+            button5.Text = "DESTROY";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(581, 504);
+            Controls.Add(button5);
+            Controls.Add(back);
+            Controls.Add(copyTo);
+            Controls.Add(Path2);
+            Controls.Add(listBox2);
+            Controls.Add(moveToLeft);
+            Controls.Add(moveToRight);
+            Controls.Add(listBox1);
+            Controls.Add(Path1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "Form1";
+            Text = "FDestoryer";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox Path1;
+        private ListBox listBox1;
+        private Button moveToRight;
+        private Button moveToLeft;
+        private ListBox listBox2;
+        private TextBox Path2;
+        private Button copyTo;
+        private Button back;
+        private Button button5;
+    }
+}
---
+++ FD/Form1.cs
@@ -0,0 +1,172 @@
+﻿namespace FD
+{
+    public partial class Form1 : Form
+    {
+        private enum LastAccessed
+        {
+            LeftListBox,
+            RightListBox
+        }
+        private LastAccessed lastAccessed;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCurrentDrivers(listBox1);
+            LoadCurrentDrivers(listBox2);
+        }
+        private void LoadCurrentDrivers(ListBox listToShow)
+        {
+            back.Enabled = false;
+
+            DriveInfo[] drivers = DriveInfo.GetDrives();
+
+            listToShow.Items.Clear();
+
+            foreach (DriveInfo driver in drivers)
+            {
+                    listToShow.Items.Add(driver);
+
+
+            }
+        }
+        private void FillListBox(ListBox list, TextBox currentTextBox)
+        {
+            if (list.SelectedItem is DriveInfo driver)
+            {
+                if (driver.IsReady)
+                {
+                    currentTextBox.Text = driver.Name;
+                    LoadDirectory(driver.RootDirectory.FullName, list);
+                }
+            }
+            else if (list.SelectedItem is DirectoryInfo directory)
+            {
+                currentTextBox.Text = directory.FullName;
+                LoadDirectory(directory.FullName, list);
+            }
+        }
+
+        private void Navigate()
+        {
+            if (lastAccessed == LastAccessed.LeftListBox)
+            {
+
+                FillListBox(listBox1, Path1);
+
+            }
+            else
+            {
+                FillListBox(listBox2, Path2);
+            }
+        }
+        private void LoadDirectory(string path, ListBox listBoxToAdd)
+        {
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+                listBoxToAdd.Items.Clear();
+
+                foreach (DirectoryInfo dir in directory.GetDirectories())
+                {
+                    listBoxToAdd.Items.Add(dir);
+                }
+
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    listBoxToAdd.Items.Add(file);
+                }
+
+                back.Enabled = true;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"ERROR: {ex.Message}");
+            }
+        }
+
+
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            string CurrentPath = string.Empty;
+            ListBox currentListBox = null;
+            TextBox textBox = null;
+            if (lastAccessed == LastAccessed.LeftListBox)
+            {
+
+                CurrentPath = Path1.Text;
+                currentListBox = listBox1;
+                textBox = Path1;
+
+
+            }
+            else
+            {
+                CurrentPath = Path2.Text;
+                currentListBox = listBox2;
+                textBox = Path2;
+            }
+
+       
+
+            if (CurrentPath.Equals(string.Empty))
+            {
+                back.Enabled = false;
+                return;
+            }
+            back.Enabled = true;
+
+            DirectoryInfo CurrentDirectory = new DirectoryInfo(CurrentPath);
+
+
+
+            DirectoryInfo ParentDirectory = CurrentDirectory.Parent;
+
+            if (ParentDirectory == null)
+            {
+                LoadCurrentDrivers(currentListBox);
+                textBox.Text = string.Empty;
+                return;
+            }
+
+            LoadDirectory(ParentDirectory.FullName, currentListBox);
+
+            if (lastAccessed == LastAccessed.LeftListBox)
+            {
+
+                Path1.Text = ParentDirectory.FullName;
+
+
+            }
+            else
+            {
+                Path2.Text = ParentDirectory.FullName;
+            }
+
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Navigate();
+
+        }
+
+        private void listBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Navigate();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lastAccessed = LastAccessed.LeftListBox;
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lastAccessed = LastAccessed.RightListBox;
+
+        }
+    }
+}
